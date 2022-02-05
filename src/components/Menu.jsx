59dca5667803500d639b43e781506ccbae0a1917
PassyahRaffi <passyah11@gmail.com>
2022-02-05 20:27:42 +0700

    update

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,37 +1,44 @@
+import React from "react";
 import ProductCard from "./ProductCard";
 
-function Menu() {
+export default function Menu() {
   return (
     <div className="mx-28">
-        <div>
-            <p className="font-['Avenir-Black'] text-red-600 font-bold text-4xl my-10">
-            Let’s Order
-            </p>
-        </div>
-        <div className="flex justify-between gap-x-8">
-            <ProductCard
-                productImage="drink1"
-                name="Ice Coffee Palm Sugar"
-                price="Rp.27.000"
-            />
-            <ProductCard
-                productImage="drink2"
-                name="Ice Coffee Green Tea"
-                price="Rp.31.000"
-            />
-            <ProductCard
-                productImage="drink3"
-                name="Hanami Latte"
-                price="Rp.29.000"
-            />
-            <ProductCard
-                productImage="drink4"
-                name="Clepon Cooffee"
-                price="Rp.28.000"
-            />
-        </div>
+      <div>
+        <p className='text-red-600 font-["Avenir-Black"] font-bold text-4xl my-10'>
+          Let's Order
+        </p>
+      </div>
+      <div className="flex justify-between gap-x-8">
+        <a href="/product">
+          <ProductCard
+            productImage="drink1"
+            name="Ice Coffee Palm Sugar"
+            price="Rp.27.000"
+          />
+        </a>
+        <a href="/product">
+          <ProductCard
+            productImage="drink2"
+            name="Ice Coffee Green tea"
+            price="Rp.31.000"
+          />
+        </a>
+        <a href="/product">
+          <ProductCard
+            productImage="drink3"
+            name="Hanami Latte"
+            price="Rp.29.000"
+          />
+        </a>
+        <a href="/product">
+          <ProductCard
+            productImage="drink4"
+            name="Clepon Coffee "
+            price="Rp.28.000"
+          />
+        </a>
+      </div>
     </div>
   );
 }
-
-export default Menu;
