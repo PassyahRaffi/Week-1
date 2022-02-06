1e2828586c931c51760027beb73c0b48e51b0981
PassyahRaffi <passyah11@gmail.com>
2022-02-06 22:17:43 +0700

    edit AddProduct.jsx

--- src/components/Admin/AddProduct.jsx
+++ src/components/Admin/AddProduct.jsx
@@ -1,42 +1,40 @@
-import React from "react";
+import React from 'react';
 
 export default function AddProduct() {
   return (
-    <div className="flex justify-between mx-28 my-10">
-      <div className="w-7/12">
+    <div className='flex justify-between mx-28 my-10'>
+      <div className='w-7/12'>
         <h4 className='text-5xl font-bold font-["Avenir-Black"] text-red-600'>
           Product
         </h4>
-        <form
-          action="/"
-          method="get"
-          className='space-y-8 my-10 font-["Avenir-Book"] text-center'
-        >
-          <input
-            type="text"
+        <form action="/" method='get' className='space-y-8 my-10 font-["Avenir-Book"] text-center'>
+          <input type="text"
             name="AddProductName"
-            placeholder="Name Product"
-            className="w-full p-3 outline outline-2 outline-red-500 focus:outline-red-700 rounded-md bg-pink-100"
+            placeholder='Name Product'
+            className='w-full p-3 outline outline-2 outline-red-500 focus:outline-red-700 rounded-md bg-pink-100' 
           />
-          <input
-            type="text"
+          <input type="text"
             name="AddProductPrice"
-            placeholder="Price"
-            className="w-full p-3 outline outline-2 outline-red-500 focus:outline-red-700 rounded-md bg-pink-100"
+            placeholder='Price'
+            className='w-full p-3 outline outline-2 outline-red-500 focus:outline-red-700 rounded-md bg-pink-100' 
           />
-          <input
-            type="text"
-            name="AddProductPhoto"
-            placeholder="Photo Product"
-            className="w-full p-3 outline outline-2 outline-red-500 focus:outline-red-700 rounded-md bg-pink-100"
-          />
-          <button className="w-9/12 py-2 rounded-md text-white text-center bg-red-600">
+          <label className='flex justify-between outline outline-2 outline-red-500 focus:outline-red-700 rounded-md bg-pink-100' htmlFor="file-upload">
+            <div className='flex justify-between w-full items-center'>
+              <input type="file" id='file-upload'
+              name="AddProductPhoto"
+              placeholder='Photo Product' className='sr-only'
+              />
+              <p className='font-["Avenir-Book"] p-3 ml-1 bg-pink-100 opacity-100 text-gray-400 text-md'>Photo Product</p>
+              <img className='h-8 mr-4' src="img/add.png" alt="" />
+            </div>
+          </label>
+          <button className='w-9/12 py-2 rounded-md text-white text-center bg-red-600'>
             Add Product
           </button>
         </form>
       </div>
-      <div className="w-4/12">
-        <img src="/img/drink1.png" alt="product" className="w-full" />
+      <div className='w-4/12'>
+        <img src="/img/drink1.png" alt="product" className='w-full' />
       </div>
     </div>
   );
